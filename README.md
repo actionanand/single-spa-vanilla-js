660e79ca4f2394938e5d3092334567b5fd9ca9c6
Anand raja <meetanandns@gmail.com>
2022-08-01 11:14:59 +0000

    vanilla js

---
+++ README.md
@@ -0,0 +1,80 @@
+# single-spa vanilla JS
+
+This is an example application that uses [single-spa-html](https://single-spa.js.org/docs/ecosystem-html-web-components/) that is enhanced with plain JavaScript.
+
+This example is modeled after a simple usecase: obtaining cookie consent from your users. The markup is relatively simple, the interactions don't do much, but some JavaScript is required to get it to work. This example highlights the following features:
+
+- using single-spa-html along with plain JavaScript
+- html template is extracted out of the js file
+- styles are included
+- transitions in and out
+
+`index.js` is commented to show how and why the code is written the way it is. To reset the UI after having "accepted", delete the `cookie-consent` localStorage value using your browser's devtools.
+
+
+### Running locally
+
+```bash
+yarn start
+```
+
+- navigate to [http://localhost:20228/](http://localhost:20228/)
+
+[standalone-single-spa-webpack-plugin](https://github.com/single-spa/standalone-single-spa-webpack-plugin) enables local development using a locally served web page.
+
+
+## How to build for production
+
+```bash
+yarn build
+```
+
+### Running in an existing root-config
+
+- `yarn start`
+- Include this module's entry in your import map
+
+  ```js
+  {
+    imports: {
+      ... // other imports
+      "@actionanand/single-spa-vanilla-js": "http://localhost:20228/index.js"
+    }
+  }
+  ```
+
+  - the webpack config outputs the app entry url for a better DX
+
+- [Register as a single-spa application](https://single-spa.js.org/docs/api/#registerapplication) in your root config
+
+  ```js
+  registerApplication({
+    name: "@actionanand/single-spa-vanilla-js",
+    app: () => System.import("@actionanand/single-spa-vanilla-js"),
+    activeWhen: ["/"],
+  });
+  ```
+
+## Cloning Guide
+
+### clone only the remote primary HEAD (default: origin/master)
+```bash
+git clone <url> --single-branch
+```
+
+### Only specific branch
+
+```bash
+git clone <url> --branch <branch> --single-branch [<folder>]
+```
+
+```bash
+git clone <url> --branch <branch> 
+```
+
+### Cloning repositories using degit
+
+```bash
+npx degit github:user/repo#branch-name <folder-name>
+```
+- master branch is default.
---
+++ src/index.js
@@ -0,0 +1,91 @@
+import singleSpaHtml from "single-spa-html"; // single-spa lifecycles helper
+import template from "./template.html"; // separate html template provides better syntax highlighting
+import styles from "./styles.css"; // CSS Modules; pitfall: ensure that your CSS is scoped else they will be *global*
+
+const ONE_MINUTE = 60000;
+
+// Use CSS modules in html template by interpolating them
+const interpolateTemplate = () => {
+  const cssModuleClassNames = Object.keys(styles).join("|");
+  const classNamesRegex = new RegExp(cssModuleClassNames, "gi");
+  const templateWithClassNames = template.replace(
+    classNamesRegex,
+    (matched) => styles[matched]
+  );
+  return templateWithClassNames;
+};
+
+const htmlLifecycles = singleSpaHtml({
+  domElementGetter: () => {
+    const id = "single-spa-application:@actionanand/single-spa-vanilla-js";
+    let container = document.getElementById(id);
+    if (!container) {
+      container = document.createElement("div");
+      container.id = id;
+      document.body.prepend(container); // single-spa automatically _appends_, but this content should be _prepended_ for accessibility
+    }
+    return container;
+  },
+  template: interpolateTemplate(),
+});
+
+/*
+This seems complicated so let's break it down:
+1. this mount function will be used by single-spa as part of the application lifecycle
+2. it wraps htmlLifecycles.mount to in order to conditionally render the html (based on prior consent);
+  this may not be needed for many applications! If instead always mount the same content this
+  could instead be implemented as `export const mount = [htmlLifecycles.mount, myOtherMountFn]`
+3. If no prior consent, then await the original mount function and bind behaviors the interactive elements
+*/
+export const mount = async (props) => {
+  // uses localStorage for convenience but could be anything to check.
+  // for example, this could be implemented with cookies if you wanted to send it back and forth to your server
+  const priorConsentOrNull = localStorage.getItem("cookie-consent");
+  const hasPriorConsent = Boolean(priorConsentOrNull); // check for prior consent set by app
+
+  if (hasPriorConsent) {
+    const { date } = JSON.parse(priorConsentOrNull);
+    const priorConsentDate = new Date(date).getTime();
+    // consent may expire so request it again if needed
+    // this also allows me to clear the value for demo purposes
+    const priorConsentExpired = priorConsentDate - Date.now() > ONE_MINUTE;
+    if (priorConsentExpired) {
+      localStorage.removeItem("cookie-consent"); // re-obtain consent if expired
+    } else {
+      return Promise.resolve(null); // don't render anything if user prior consent & it has not expired
+    }
+  }
+  await htmlLifecycles.mount(props); // wait for single-spa to mount the application
+
+  // after app mount, bind behaviors the interactive elements with plain JavaScript
+  const dialog = document.querySelector("#cookie-consent"),
+    noSellCheckbox = dialog?.querySelector("#cookie-consent-no-sell"),
+    acceptBtn = dialog?.querySelector("#cookie-consent-accept");
+
+  if (!dialog || !noSellCheckbox || !acceptBtn) return; // sanity checks
+
+  // bind and handle click event on button
+  acceptBtn.addEventListener("click", function handleConsentAction() {
+    // create consent data object
+    const consent = JSON.stringify({
+      date: new Date().toJSON(),
+      noSell: noSellCheckbox.checked,
+    });
+    localStorage.setItem("cookie-consent", consent);
+    dialog.classList.add(styles.hide); // animate out
+  });
+
+  // remove hide class so that dialog animates in
+  setTimeout(function onInit() {
+    dialog.classList.remove(styles.hide);
+  });
+
+  dialog.addEventListener("transitionend", function handleTransitionEnd() {
+    // listen for when animation ends and set hidden attribute so that they remain in sync
+    dialog.classList.contains(styles.hide) && dialog.setAttribute("hidden", "");
+  });
+
+  // TODO: implement a way to revoke consent (execise left to reader)
+};
+
+export const { bootstrap, unmount } = htmlLifecycles; // export other lifecycles as-is
